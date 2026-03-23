81f81430b444e15ab4a04e40ff179908685fc5fa
traderpresley0-boop <traderpresley0@gmail.com>
2026-03-23 20:34:17 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from api.routes.entrada import entrada_bp
 
 app = Flask(__name__)
+CORS(app)
 
-# Registrar rotas
 app.register_blueprint(entrada_bp)
 
 if __name__ == "__main__":
