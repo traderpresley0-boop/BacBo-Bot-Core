6a58424b5b179394e95970c5494dc204d76eb41b
traderpresley0-boop <traderpresley0@gmail.com>
2026-03-23 22:20:13 +0100

    Update dados.py

--- src/core/dados.py
+++ src/core/dados.py
@@ -1,1 +1,9 @@
+class Dados:
+    def __init__(self):
+        self.historico = []
 
+    def obter(self):
+        # Retorna um dado simulado da rodada
+        rodada = {"bolinha": "vermelha", "numero": 1}
+        self.historico.append(rodada)
+        return rodada
