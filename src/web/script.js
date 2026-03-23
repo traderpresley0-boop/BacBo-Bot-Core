329d144d8d70eac2f421fad5efaca1efbd2e0716
traderpresley0-boop <traderpresley0@gmail.com>
2026-03-23 22:29:49 +0100

    Update script.js

--- src/web/script.js
+++ src/web/script.js
@@ -1,1 +1,6 @@
-
+fetch('http://127.0.0.1:5000/status')
+.then(response => response.json())
+.then(data => {
+    document.getElementById('status').innerText = data.status;
+})
+.catch(error => console.log('Erro:', error));
