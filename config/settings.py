aed320380f12d97b4441949805fb628850f297ef
traderpresley0-boop <traderpresley0@gmail.com>
2026-03-23 16:36:23 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,26 @@
+# config/settings.py
 
+# Valores atualizados da banca e aposta
+BANCA_INICIAL = 5000
+APOSTA_BASE = 1000
+
+# Limites de risco atualizados
+LIMITE_PERDA = 2000
+META_LUCRO = 3000
+
+# Estratégias ativas
+ESTRATEGIAS_ATIVAS = [
+    "racha_curta",
+    "racha_media",
+    "racha_longa",
+    "racha_extra_longa",
+    "alternancia_simples",
+    "alternancia_dupla",
+    "alternancia_tripla",
+    "zig_zag",
+    "racha_alternancia",
+    "alternancia_racha",
+    "predominio",
+    "sequencia_concentrada",
+    "tendencia_quebra"
+]
