4bd29c00711e40188dcd9af17964ee2407d6dd4e
traderpresley0-boop <traderpresley0@gmail.com>
2026-03-23 16:24:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# main.py
 
+from core.estado import atualizar_estado, estado
+from core.dados import validar_entrada
+
+def mostrar_estado():
+    print("\n=== ESTADO ATUAL ===")
+    print("Total:", estado["total_jogadas"])
+    print("Contagem:", estado["contagem"])
+    print("Sequência:", estado["sequencia_atual"])
+    print("====================\n")
+
+while True:
+    entrada = input("Resultado (P/B): ")
+    resultado = validar_entrada(entrada)
+    
+    if not resultado:
+        print("Entrada inválida")
+        continue
+
+    atualizar_estado(resultado)
+    mostrar_estado()
