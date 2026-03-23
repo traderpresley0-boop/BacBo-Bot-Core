16a50b540ed5dc6d3afa5515958ef85f78e7c280
traderpresley0-boop <traderpresley0@gmail.com>
2026-03-23 16:23:10 +0100

    Update dados.py

--- core/dados.py
+++ core/dados.py
@@ -1,1 +1,12 @@
+# core/dados.py
+from datetime import datetime
 
+def validar_entrada(entrada):
+    entrada = entrada.upper()
+    if entrada not in ["P", "B"]:
+        return None
+
+    return {
+        "valor": entrada,
+        "timestamp": datetime.now()
+    }
